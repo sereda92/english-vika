0b92cddd4946d2f630ff209a214995fd4f929c61
sereda_pavel <assakay92@gmail.com>
2024-08-18 23:56:52 +0300

    new edit

--- src/js/index.js
+++ src/js/index.js
@@ -20,19 +20,38 @@ const burgerBtn = document.querySelector('.burger__btn')
 const burgerMain = document.querySelector('.burger')
 const body = document.querySelector('body')
 const header = document.querySelector('.header')
+const navLinks = document.querySelectorAll('.burger__item')
 
-burgerBtn.addEventListener('click', () => {
-    burgerMain.classList.toggle('burger_active')
+burgerBtn.addEventListener('click', toggleMenu);
+burgerBtn.addEventListener('touchstart', toggleMenu);
+
+function toggleMenu(e) {
+    e.preventDefault();
+    burgerMain.classList.toggle('burger_active');
     if(burgerMain.classList.contains('burger_active')){
-        body.classList.add('body_non_scroll')
-        burgerBtn.classList.add('burger__btn_active')
-        header.classList.add('header_burger_active')
-    }else{
-        body.classList.remove('body_non_scroll')
-        burgerBtn.classList.remove('burger__btn_active')
-        header.classList.remove('header_burger_active')
+        body.classList.add('body_non_scroll');
+        burgerBtn.classList.add('burger__btn_active');
+        header.classList.add('header_burger_active');
+    } else {
+        body.classList.remove('body_non_scroll');
+        burgerBtn.classList.remove('burger__btn_active');
+        header.classList.remove('header_burger_active');
     }
-})
+}
+
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (burgerMain.classList.contains('burger_active')) {
+            burgerMain.classList.remove('burger_active');
+            body.classList.remove('body_non_scroll');
+            burgerBtn.classList.remove('burger__btn_active');
+            header.classList.remove('header_burger_active');
+        }
+    });
+});
+
 
 //-------------------------------------------
 
@@ -88,5 +107,52 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+// -------------------
+// const insideArrow2 = document.querySelector('.inside_arrow_2')
+// const whatsInsideBody = document.querySelector('.whats-inside_body')
 
+
+// insideArrow2.addEventListener('click', () => {
+//     insideArrow2.classList.toggle('inside_arrow_2_active')
+//     whatsInsideBody.classList.toggle('inside_visible')
+// })
+
+//---------------------
+const whatsInsideBody = document.querySelector('.whats-inside_body');
+const triggerElement = document.querySelector('.inside_arrow_2');
+
+triggerElement.addEventListener('click', () => {
+    if (!whatsInsideBody.classList.contains('inside_visible')) {
+        whatsInsideBody.style.height = `${whatsInsideBody.scrollHeight}px`;
+        whatsInsideBody.classList.add('inside_visible');
+        triggerElement.classList.add('inside_arrow_2_active')
+        // if(!triggerElement.classList.contains('inside_arrow_2_active')){
+        // }else{
+        //     triggerElement.classList.remove('inside_arrow_2_active')
+        // }
+        
+
+        whatsInsideBody.addEventListener('transitionend', () => {
+            // После завершения анимации делаем высоту auto, чтобы блок мог адаптироваться к изменению контента
+            if (whatsInsideBody.classList.contains('inside_visible')) {
+                whatsInsideBody.style.height = 'auto';
+            }
+        }, { once: true });
+
+    } else {
+        whatsInsideBody.style.height = `${whatsInsideBody.scrollHeight}px`;
+
+        // Добавляем задержку, чтобы CSS мог отследить изменение высоты перед сворачиванием
+        setTimeout(() => {
+            whatsInsideBody.style.height = '0px';
+            whatsInsideBody.classList.remove('inside_visible');
+            triggerElement.classList.remove('inside_arrow_2_active')
+        }, 0);
+    }
+});
+
+// Плавный переход высоты
+whatsInsideBody.style.transition = 'height 0.3s ease-in-out, opacity 0.4s ease-in-out';
+
+// --------------
 
